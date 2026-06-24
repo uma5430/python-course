5d6369563444a9518c40f478d2f43a73929c5bef
uma5430 <umapatil@gmail.com>
2026-06-24 17:25:58 +0530

    week 1 day 2

--- task1.py
+++ task1.py
@@ -1,14 +1,1 @@
-name="uma patil"
-age=20
-email="umapatil@gamil.com"
-address="jivaba park,kaneri"
-std="BCA III"
-mob_no=7666357455
-percentage=74.80
-print(type(name),name)
-print(type (age),age)
-print(type (email),email)
-print(type (address),address)
-print(type (std),std)
-print(type (mob_no),mob_no)
-print(type (percentage),percentage)
+print("hello world")
--- task2.py
+++ task2.py
@@ -1,5 +1,14 @@
-age=int(input("enter your age:"));
-if age>=18:
-    print("voter eligible")
-else:
-    print("voter not eligible")
+name="uma patil"
+age=20
+email="umapatil@gamil.com"
+address="jivaba park,kaneri"
+std="BCA III"
+mob_no=7666357455
+percentage=74.80
+print(type(name),name)
+print(type (age),age)
+print(type (email),email)
+print(type (address),address)
+print(type (std),std)
+print(type (mob_no),mob_no)
+print(type (percentage),percentage)
